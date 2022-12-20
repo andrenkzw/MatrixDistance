dfd0a759868ec9f0df51b3264113a04abdd6c7b5
Andre Nakazawa <andrenkzw@gmail.com>
2022-12-20 07:05:32 -0300

    Questao 1 - Generalizando leitura repetida

--- Program.cs
+++ Program.cs
@@ -5,23 +5,23 @@ namespace MatrixDistance;
 class Program
 {
     static int LerTamanhoUsuario() {
-        int n;
-        Console.WriteLine($"Insira o número de cidades:");
-        while (!int.TryParse(Console.ReadLine(), out n)) {
-            Console.WriteLine("Erro na leitura.");
-            Console.WriteLine($"Insira o número de cidades:");
-        }
-        return n;        
+        return Utils.LerInt(
+            () => {
+                Console.WriteLine($"Insira o número de cidades:");
+                bool sucesso = int.TryParse(Console.ReadLine(), out int n);
+                return (sucesso, n);
+            }
+        );
     }
 
     static int LerDistUsuario(int i, int j) {
-        int dist;
-        Console.WriteLine($"Insira a distância entre a cidade {i} e a cidade {j}:");
-        while (!int.TryParse(Console.ReadLine(), out dist)) {
-            Console.WriteLine("Erro na leitura.");
-            Console.WriteLine($"Insira a distância entre a cidade {i} e a cidade {j}:");
-        }
-        return dist;
+        return Utils.LerInt(
+            () => {
+                Console.WriteLine($"Insira a distância entre a cidade {i} e a cidade {j}:");
+                bool sucesso = int.TryParse(Console.ReadLine(), out int dist);
+                return (sucesso, dist);
+            }
+        );
     }
 
     static int[,] LerMatrizUsuario(int n = 5) {
@@ -37,16 +37,16 @@ class Program
     }
 
     static int[] LerPercursoUsuario(int n = 5) {
-        Console.WriteLine($"Insira o percurso com as cidades separadas por espaço:");
-        string[] textoPercurso = (Console.ReadLine() ?? "").Split(" ");
-        int[] percurso = textoPercurso.Select(x => (int.TryParse(x, out int y) ? y : -1)).ToArray();
-        while (percurso.Any(x => x <= 0 || x > n)) {
-            Console.WriteLine("Erro na leitura.");
-            Console.WriteLine($"Insira o percurso com as cidades separadas por espaço:");
-            textoPercurso = (Console.ReadLine() ?? "").Split(" ");
-            percurso = textoPercurso.Select(x => (int.TryParse(x, out int y) ? y : -1)).ToArray();
-        }
-        return percurso.ParaSistema();
+        return Utils.LerArray(
+            () => {
+                Console.WriteLine($"Insira o percurso com as cidades separadas por espaço:");
+                string[] textoPercurso = (Console.ReadLine() ?? "").Split(" ");
+                return  textoPercurso.Select(x => (int.TryParse(x, out int y) ? y : -1)).ToArray();
+            },
+            (int[] percurso) => {
+                return !percurso.Any(x => x <= 0 || x > n);
+            }
+        ).ParaSistema();
     }
 
     static int CalculaDistancia(int[,] matriz, int[] percurso)
@@ -57,6 +57,6 @@ class Program
         int n = LerTamanhoUsuario();
         int[,] matriz = LerMatrizUsuario(n);
         int[] percurso = LerPercursoUsuario(n);
-        Console.WriteLine($"A distância percorrida no percurso {string.Join(" ", percurso.Select(x => x+1))} é {CalculaDistancia(matriz, percurso)} km.");
+        Console.WriteLine($"A distância percorrida no percurso {string.Join(" ", percurso.ParaUsuario())} é {CalculaDistancia(matriz, percurso)} km.");
     }
 }
--- Utils.cs
+++ Utils.cs
@@ -15,4 +15,22 @@ public static class Utils
     
     public static int[] ParaSistema(this int[] arrayUsuario)
         => arrayUsuario.Select(x => x-1).ToArray();
+    
+    public static int LerInt(Func<(bool,int)> ler) {
+        (bool sucesso, int resultado) = ler();
+        while (!sucesso) {
+            Console.WriteLine("Erro na leitura.");
+            (sucesso, resultado) = ler();
+        }
+        return resultado;
+    }
+
+    public static int[] LerArray(Func<int[]> ler, Func<int[], bool> testar) {
+        int[] resultado = ler();
+        while (!testar(resultado)) {
+            Console.WriteLine("Erro na leitura.");
+            resultado = ler();
+        }
+        return resultado;
+    }
 }
