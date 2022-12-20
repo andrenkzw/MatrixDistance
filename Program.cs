834e8e43520214a00e2d41aae0ffe4e773d1943c
Andre Nakazawa <andrenkzw@gmail.com>
2022-12-20 06:18:05 -0300

    Questao 1 - Criacao da classe Utils

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ class Program
         int[,] matriz = new int[n,n];
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
-                int dist = LerDistUsuario(i+1, j+1);
+                int dist = LerDistUsuario(i.ParaUsuario(), j.ParaUsuario());
                 matriz[i,j] = dist;
                 matriz[j,i] = dist;
             }
@@ -39,14 +39,14 @@ class Program
     static int[] LerPercursoUsuario(int n = 5) {
         Console.WriteLine($"Insira o percurso com as cidades separadas por espaço:");
         string[] textoPercurso = (Console.ReadLine() ?? "").Split(" ");
-        int[] percurso = textoPercurso.Select(x => (int.TryParse(x, out int y) ? y-1 : -1)).ToArray();
-        while (percurso.Any(x => x < 0 || x >= n)) {
+        int[] percurso = textoPercurso.Select(x => (int.TryParse(x, out int y) ? y : -1)).ToArray();
+        while (percurso.Any(x => x <= 0 || x > n)) {
             Console.WriteLine("Erro na leitura.");
             Console.WriteLine($"Insira o percurso com as cidades separadas por espaço:");
             textoPercurso = (Console.ReadLine() ?? "").Split(" ");
-            percurso = textoPercurso.Select(x => (int.TryParse(x, out int y) ? y-1 : -1)).ToArray();
+            percurso = textoPercurso.Select(x => (int.TryParse(x, out int y) ? y : -1)).ToArray();
         }
-        return percurso;
+        return percurso.ParaSistema();
     }
 
     static int CalculaDistancia(int[,] matriz, int[] percurso)
---
+++ Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace MatrixDistance;
+public static class Utils
+{
+    public static int ParaUsuario(this int intSistema)
+        => intSistema+1;
+    
+    public static int ParaSistema(this int intUsuario)
+        => intUsuario-1;
+
+    public static int[] ParaUsuario(this int[] arraySistema)
+        => arraySistema.Select(x => x+1).ToArray();
+    
+    public static int[] ParaSistema(this int[] arrayUsuario)
+        => arrayUsuario.Select(x => x-1).ToArray();
+}
